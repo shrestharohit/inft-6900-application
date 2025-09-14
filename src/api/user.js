0c12c562e25580e1399e67a83361faaf13caf3a2
Rohit Shrestha <rohitshr98@gmail.com>
2025-09-14 13:11:57 +1000

    :bug: fix: remove extra slash in api call

--- src/api/user.js
+++ src/api/user.js
@@ -3,7 +3,7 @@ import { API_BASE_URL } from "./config";
 
 export const registerUser = async (userData) => {
   try {
-    const response = await axios.post(`${API_BASE_URL}//api/auth/register`, userData);
+    const response = await axios.post(`${API_BASE_URL}/api/auth/register`, userData);
     return response.data;
   } catch (error) {
     console.error("Error registering user:", error);
