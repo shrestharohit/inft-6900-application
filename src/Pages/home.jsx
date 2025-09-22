1e268d39ac0a6c3ff8ef15c34e142d3d87ff0132
Rohit Shrestha <rohitshr98@gmail.com>
2025-09-22 13:18:52 +1000

    fix: create a common.js file

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,63 +1,10 @@
 import { Link } from "react-router-dom";
 
-import webDevImg from "../assets/Images/webdevremovebg.png";
-import dataAnalyticsImg from "../assets/Images/dataanaremovebg.png";
-import aiMlImg from "../assets/Images/aimlremovebg.png";
-
-import techSkillsImg from "../assets/Images/techskills.png";
-import analyticalSkillsImg from "../assets/Images/analyticalskills.png";
-import businessSkillsImg from "../assets/Images/businessskills.png";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 import beforeAuthLayout from "../components/BeforeAuth";
+import { popularCourses, trendingPathways } from "../utils/common";
 
 function Home() {
-  const popularCourses = [
-    {
-      name: "Web Development",
-      img: webDevImg,
-      link: "/courses/web-development",
-    },
-    {
-      name: "Data Analytics",
-      img: dataAnalyticsImg,
-      link: "/courses/data-analytics",
-    },
-    { name: "AI & Machine Learning", img: aiMlImg, link: "/courses/ai-ml" },
-  ];
 
-  const trendingPathways = [
-    {
-      name: "Tech Skills",
-      link: "/pathway/tech-skills",
-      description: "Master coding and DevOps skills.",
-      img: techSkillsImg,
-      courses: [
-        { name: "Coding", link: "/courses/coding" },
-        { name: "DevOps", link: "/courses/devops" },
-      ],
-    },
-    {
-      name: "Analytical Skills",
-      link: "/pathway/analytical-skills",
-      description: "Learn Big Data and Power BI.",
-      img: analyticalSkillsImg,
-      courses: [
-        { name: "Big Data", link: "/courses/bigdata" },
-        { name: "Power BI", link: "/courses/powerbi" },
-      ],
-    },
-    {
-      name: "Business Skills",
-      link: "/pathway/business-skills",
-      description: "Build Accounting and Finance expertise.",
-      img: businessSkillsImg,
-      courses: [
-        { name: "Accounting", link: "/courses/accounting" },
-        { name: "Finance", link: "/courses/finance" },
-      ],
-    },
-  ];
 
   return (
     <>
