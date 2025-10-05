b9d1cdf6bff9738281a04af5f4604fa15f83ede1
Gargi <gargidarwhatkar5699@gmail.com>
2025-10-05 22:53:56 +1100

    verify passowrd

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -26,7 +26,7 @@ function Home() {
     };
 
     return (
-        <div className="bg-gray-50 font-inter pb-12">
+        <div className="bg-gray-50 font-inter pb-12 pt-12">
             {/* Hero Section */}
             <section className="bg-gradient-to-r from-[#1f2a60] to-[#4856a6] text-white rounded-xl my-8 max-w-[1150px] mx-auto shadow-lg">
                 <div className="text-center py-20 px-6 max-w-3xl mx-auto">
